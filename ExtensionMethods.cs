4ce4771db75e291e4cfb5cc262bc3cc7a8249e88
k76 <krg0077++src@gmail.com>
2021-01-26 06:14:04 -0800

    Add ExtensionMethods class with List.Shift

---
+++ ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace _0G
+{
+    public static class ExtensionMethods
+    {
+        public static void Shift<T>(this List<T> list, T item, int indexDelta)
+        {
+            int index = list.IndexOf(item);
+            list.RemoveAt(index);
+            list.Insert(index + indexDelta, item);
+        }
+    }
+}
