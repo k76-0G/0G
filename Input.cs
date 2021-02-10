8809ef7359216ccc32432951993c35615d754180
k76 <krg0077++src@gmail.com>
2021-02-10 03:29:01 -0800

    Add Input placeholder class (with InputKey enum)

---
+++ Input.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _0G
+{
+    public class Input : MonoBehaviour
+    {
+        // TODO: should this be static or instanced?
+        // input should reference a player object
+        // as well as InputKey
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ InputKey.cs
@@ -0,0 +1,13 @@
+﻿namespace _0G
+{
+    public enum InputKey
+    {
+        None = 0,
+        UISubmit = 1,
+        UICancel = 2,
+        UIUp = 3,
+        UIDown = 4,
+        UILeft = 5,
+        UIRight = 6,
+    }
+}
